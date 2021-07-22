526c7a3974254ea336c83a0604964f38ea94e60d
DerZade <DerZade@gmail.com>
2021-07-22 15:16:50 +0200

    Yo

--- tools/topicPills.mjs
+++ tools/topicPills.mjs
@@ -2,7 +2,7 @@ import { generateQueryLink } from './utils.mjs';
 import { ORG_NAME } from './const.mjs';
 
 
-const TOPIC_LINK_PATTERN = /<span .*data-topic="([a-z0-9-_]+)".*>.*<\/span>/gi
+const TOPIC_LINK_PATTERN = /<span [^>]*data-topic="([a-z0-9-_]+)"[^>]*>[^<]*<\/span>/gi
 
 /**
  * @param {string} topic Topic
